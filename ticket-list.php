6e8eaa92d8975a830433025db9e180e9b4715890
Jacob Weisz <inbox@jacobweisz.com>
2025-04-22 23:17:06 -0500

    Display ticket message in modal

--- ticket-list.php
+++ ticket-list.php
@@ -44,7 +44,7 @@
                                 <td>".$val['tt_status']."</td>
                                 <td>".date("d F Y", strtotime($val['tt_created']))."<br>".date("H:i:s A", strtotime($val['tt_created']))."</td>
                                 <td>
-                                    <a class='btn btn-default' data-toggle='modal' data-target='#ed-".$key."'>Option</a>
+                                    <a class='btn btn-default' data-toggle='modal' data-target='#ed-".$key."'>Open</a>
                                 </td>
                             </tr>
                         ";
@@ -54,14 +54,20 @@
                             <form class="modal-content" method="POST">
                                 <div class="modal-header">
                                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                    <h4 class="modal-title">Change status <?=$val['tt_subject'];?></h4>
+                                    <h4 class="modal-title">Ticket #<?=$val['tt_id'];?></h4>
                                 </div>
-                                <div class="modal-body">                    
+                                <div class="modal-body">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <label for=""><?=$val['tt_subject'];?></label>
+                                            <p><?=$val['tt_message'];?></p><br />
+                                        </div>
+                                    </div>
                                     <div class="row">
                                         <div class="col-md-8 form-group">
-                                            <label for="">Status:</label>
+                                            <label for="">Change Status:</label>
                                             <select name="status" class="form-control" required="true">
-                                                <option value="<?=$val['tt_status'];?>">SELECTED : <?=$val['tt_status'];?></option>
+                                                <option value="<?=$val['tt_status'];?>">CURRENT: <?=$val['tt_status'];?></option>
                                                 <option value="NEW">NEW</option>
                                                 <option value="PROCESS">PROCESS</option>
                                                 <option value="PENDING">PENDING</option>
